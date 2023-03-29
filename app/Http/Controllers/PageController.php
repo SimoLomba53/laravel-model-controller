b138c343b95ba71f8e5337fa43376dc28d0faa67
SimoLomba53 <simolomba253@gmail.com>
2023-03-29 16:04:32 +0200

    correzione pdo

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
     public function homepage(){
+        $movies= Movie::all();
+        dd($movies);
         return view ('homepage');
     }
 }
